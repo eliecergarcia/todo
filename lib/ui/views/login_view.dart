ddff745368f185f5a3839ee1b0d94f19c3301820
eliecer garcia <absalonsubzero@gmail.com>
2023-05-30 20:10:18 -0600

    se agrega vista del login para el proyecto

--- lib/ui/views/login_view.dart
+++ lib/ui/views/login_view.dart
@@ -6,8 +6,59 @@ class LoginView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Text('data'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // email
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: size.width * 0.1,
+                vertical: size.height * 0.05,
+              ),
+              child: TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  labelText: 'email',
+                  labelStyle: TextStyle(
+                    color: Color(0xFFBEBCBC),
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                onChanged: (value) {},
+              ),
+            ),
+
+            // password
+            Padding(
+              padding: EdgeInsets.only(
+                  left: size.width * .1,
+                  right: size.width * 0.1,
+                  bottom: size.height * 0.05),
+              child: TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  labelStyle: TextStyle(
+                    color: Color(0xFFBEBCBC),
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                onChanged: (value) {},
+              ),
+            ),
+            // button
+            ElevatedButton(
+              onPressed: () => {},
+              child: const Text(
+                'Iniciar sesion',
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
